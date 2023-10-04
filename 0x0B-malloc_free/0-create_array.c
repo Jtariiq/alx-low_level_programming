976c343c94e359e1cadebcae037633bceff2fcb4
Jtariq <jtariq.saidi@gmail.com>
2023-10-04 11:08:23 -0700

    malloc,free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of arra to create
+ * @c: char to initialize array
+ * Return: pointer to the array(success)
+ */
+
+char *create_array(unsigned int size, char c)
+{
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_strdup - copies string given as a paraameter
+ * @str: string to duplicate
+ * Return: pointer to the copied string
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
